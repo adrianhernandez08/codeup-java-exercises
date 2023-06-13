74e6fa2ea1f40eb969a3064d182f094b3438738b
Adrian Hernandez <adrian.c.hernandez08@gmail.com>
2023-06-13 14:48:36 -0500

    syntax, types, and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,85 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-    }
-}
+
 
 // I am a single line comment
 
 /* I take up one line */
 
-/**
- * I take up
- * multiple
- * lines
- */
+
+        int id;
+        boolean isLoggedIn;
+        float bankAccountBalance;
+
+
+        int theAnswer = 42;
+        String question = "What is your favorite color?";
+
+        System.out.println(question);
+
+        System.out.println("the answer is " + theAnswer);
+
+    //1//
+        int myFavoriteNumber = 17;
+    //2//
+//        String myString = "23";
+    //3//
+//        char myString = 23;
+        //4//
+        String myString = "23";
+        System.out.println(myString);
+    //5//
+//    long myNumber =
+//            System.out.println(myNumber);
+    //6//
+//        long myNumber = 3.14;
+//                System.out.println(myNumber);
+    //7//
+//        long myNumber = 123L;
+//                System.out.println(myNumber);
+    //8//
+//        long myNumber = 123;
+//                System.out.println(myNumber);
+
+    //9//
+        float   myNumber = 123;
+                System.out.println(myNumber);
+    //10//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+    //11//
+//    int class =
+
+//    //12//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+
+    //13//
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+        System.out.println(y);
+
+    //14//
+        Integer.MAX_VALUE
+
+    }
+}
